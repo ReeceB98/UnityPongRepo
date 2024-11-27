2d003c4daaad258679c4e49b997f7651a4242b79
Reece <Reece2.Bellchambers@live.uwe.ac.uk>
2024-11-27 21:03:51 +0000

    Player can now move with velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,22 +3,32 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // Player rigidbody2D
     [SerializeField] private Rigidbody2D rb2d;
 
+    // Player Inputs
     private InputAction moveAction;
 
+    // Player movement vector
     private Vector2 moveValue;
+
+    // Players overall speed
+    [SerializeField] private float playerSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        // Getting key components in game object
+        rb2d = GetComponent<Rigidbody2D>();
+
+        // Set up player inputs
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        // Read Move value as vector2 and apply rb2d velocity to make player move
         moveValue = moveAction.ReadValue<Vector2>();
-        Debug.Log(moveValue);
+        rb2d.linearVelocity = moveValue * playerSpeed;
     }
 }
