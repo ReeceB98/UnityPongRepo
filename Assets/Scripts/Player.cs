f83926330441603a981b441157bf5e0734a40aa4
Reece <Reece2.Bellchambers@live.uwe.ac.uk>
2024-11-27 20:42:54 +0000

    Visual Studio Intellisense problem fixed & Input system is now detecting player inputs
    Visual Studio Intellisense was fixed by modifying it in the VS installer to integrate with Unity. Input System now detects player up and down inputs in game.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb2d;
+
+    private InputAction moveAction;
+
+    private Vector2 moveValue;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        moveValue = moveAction.ReadValue<Vector2>();
+        Debug.Log(moveValue);
     }
 }
