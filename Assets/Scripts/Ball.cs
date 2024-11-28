d52933ef4dee8669945e1fcb9bdb4938e258da32
Reece <Reece2.Bellchambers@live.uwe.ac.uk>
2024-11-28 22:03:30 +0000

    Ball script set up & Ball is now moving with force

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Balls rigidbody
+    private Rigidbody2D rb2d;
+
+    // Ball speed
+    [SerializeField] private float ballSpeed;
+
+    // Ball direction using vectors
+    private Vector2 moveValue;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        moveValue = new Vector2(-1.0f, 0.0f);
+        rb2d.linearVelocity = ballSpeed * Time.fixedDeltaTime * moveValue;
+    }
+}
