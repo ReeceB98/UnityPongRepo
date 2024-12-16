e1d463cd4e23c5317fe0115ed73241b25291fc35
Reece <Reece2.Bellchambers@live.uwe.ac.uk>
2024-12-16 22:23:08 +0000

    Cleaned up code in all classes

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -2,20 +2,28 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
+    // ball transform
     private Transform ballTransform;
 
+    // How fast the computer moves
     [SerializeField] private float aiSpeed;
 
+    // Computer Rigidbody
     private Rigidbody2D rb2d;
 
+    // Ball class
     private Ball ball;
 
+    // Turn the AI on/off
     private bool isCompOn = true;
+
+    private float zero = 0.0f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
+        // Get the components needed
         ball = GameObject.FindFirstObjectByType<Ball>();
         ballTransform = GameObject.Find("Ball").transform;
         rb2d = GetComponent<Rigidbody2D>();
@@ -25,16 +33,19 @@ public class AI : MonoBehaviour
     {
         if (isCompOn)
         {
+            // Computer speed
             float step = aiSpeed * Time.fixedDeltaTime;
 
+            // Computer will go up towards the ball
             if (ballTransform.position.y > transform.position.y)
             {
-                rb2d.linearVelocity = new Vector2(0.0f, step);
+                rb2d.linearVelocity = new Vector2(zero, step);
             }
 
+            // Computer will go down towards the ball
             if (ballTransform.position.y < transform.position.y)
             {
-                rb2d.linearVelocity = new Vector2(0.0f, -step);
+                rb2d.linearVelocity = new Vector2(zero, -step);
             }
         }
     }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,44 +3,22 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerGoal;
-    [SerializeField] private GameObject computerGoal;
-
+    // Score values
     [SerializeField] private float playerScore;
     [SerializeField] private float compScore;
 
+    // Scores texts
     [SerializeField] private Text playerScoreText;
     [SerializeField] private Text compScoreText;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start()
-    {
-        playerGoal = GameObject.Find("PlayerGoal");
-        computerGoal = GameObject.Find("ComputerGoal");
-
-        //playerScoreText.text = playerScore.ToString();
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        //playerScoreText.text = playerScore.ToString();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.name == "PlayerGoal")
-        {
-            Debug.Log("Player's Goal");
-        }
-    }
-
+    // Set the score for the player
     public void SetPlayerScore(int score)
     {
         playerScore += score;
         playerScoreText.text = playerScore.ToString();
     }
     
+    // Set the score for the computer
     public void SetComputerScore(int score)
     {
         compScore += score;
